802e9e1fc77be833bb117fb6efacacee620faa28
LeeKennedy <mail@leekennedy.id.au>
2015-03-06 20:35:38 +1100

    Added BP script

---
+++ BP.R
@@ -0,0 +1,26 @@
+library(dplyr)
+library(ggplot2)
+library(lubridate)
+library(tidyr)
+data <- read.csv("BP.csv", as.is=TRUE, header=TRUE, skip=18)
+
+data1 <- select(data, everything())%>%
+        mutate(Systolic = as.numeric(as.character(X)), Diastolic = as.numeric(as.character(X.1)))
+
+data1 <- na.omit(data1)
+
+data2 <- data1[,c(1,16,17)]
+
+data2$Date <- dmy(data2$Date)
+
+data3 <- gather(data2, Type, Reading, -Date)
+
+plot <- ggplot(data3, aes(Date, Reading, colour = Type)) +
+        geom_point(size=4, shape=21) +
+        geom_smooth(lwd=1) +
+        scale_colour_brewer(palette="Set1") +
+        theme_bw() +
+        theme(panel.grid.major = element_line(size = 0.5, color = "grey"), 
+              axis.line = element_line(size = 0.7, color = "black"), 
+              text = element_text(size = 14))
+plot
