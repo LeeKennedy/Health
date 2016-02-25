05e6f73ed59df2fd4fe7f08368b17b55bb8420db
LeeKennedy <mail@leekennedy.id.au>
2016-02-25 22:28:51 +1100

    Added desirable range rectangle

--- scripts/DTS.challenge.R
+++ scripts/DTS.challenge.R
@@ -11,16 +11,15 @@ cache("weight2")
 
 #Weight 
 plot_wt <- ggplot(weight2, aes(Date,Weight)) + 
+                geom_rect(aes(ymin = 85, ymax = 88, xmin = weight2$Date[1], xmax = weight2$Date[120]), fill = "grey80") +
                 geom_point(shape = 21, fill = "cornflowerblue", colour = "blue", size = 8) + 
                 geom_smooth(method=loess, colour = "darkgreen", size = 2) + 
                 geom_hline(yintercept = 95.3, lty=2, col="red") +
-                geom_hline(yintercept = 85, lty=2, col="red") +
+                #geom_hline(yintercept = 85, lty=2, col="red") +
                 geom_vline(xintercept=as.numeric(weight2$Date[71]), lty=2, col="red") +
                 theme_bw() +
                 scale_y_continuous(breaks = seq(82, 96),1) +
                 ggtitle("Weight since DTS Greatest Loser Challenge started...\n") + 
-                theme(plot.title = element_text(size = 18))
-               
+                theme(plot.title = element_text(size = 18)) 
 plot_wt
-
 
