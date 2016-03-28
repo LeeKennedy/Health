659db4ffb9c9839889b139d11a17998fd3289a0b
LeeKennedy <mail@leekennedy.id.au>
2016-03-28 21:57:43 +1100

    Straggler

--- scripts/PSA.R
+++ scripts/PSA.R
@@ -1,14 +1,13 @@
 library(ProjectTemplate)
 load.project()
 
-cholesterol <- read_excel("rawdata/Healthcheck.xlsx", sheet=3)
+psa <- read_excel("rawdata/Healthcheck.xlsx", sheet=3)
 
-cholesterol <- na.omit(cholesterol[,c(1,2,4,5)])
+psa <- na.omit(psa[,c(1,7)])
 
-chol_long <- gather(data=cholesterol,key=Type,value=Level,na.rm=FALSE,Chol,HDL,LDL)
-
-chol_plot <- ggplot(chol_long, aes(x=Date, y = Level, group = Type, fill = Type)) +
-        geom_point(size = 4, shape = 21, colour = "black") +
-        geom_line(lwd = 1, aes(colour = Type)) +
-        labs(title = "Cholesterol\n")
-chol_plot
+psa_plot <- ggplot(psa, aes(x=Date, y = PSA)) +
+        geom_point(size = 4, shape = 21, fill = "cornflowerblue", colour = "black") +
+        geom_line(lwd = 1) +
+        labs(title = "PSA\n") +
+        scale_y_continuous(limits = c(0,1))
+psa_plot
