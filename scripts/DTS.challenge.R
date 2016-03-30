ff72e941ba8053819189e219f6b5de10d684c195
LeeKennedy <mail@leekennedy.id.au>
2016-03-30 17:22:50 +1100

    Rerun - am I saving too much stuff?

--- scripts/DTS.challenge.R
+++ scripts/DTS.challenge.R
@@ -4,15 +4,15 @@ load.project()
 weight <- read_excel("rawdata/Healthcheck.xlsx", sheet=1)
 weight2 <- weight %>%
         filter(Date >"2015-11-30") %>%
-        filter(Date <"2016-03-29")
+        filter(Date <"2016-04-30")
 cache("weight2")
 
 #dev.off()
 
 #Weight 
 plot_wt <- ggplot(weight2, aes(Date,Weight)) + 
-                geom_rect(aes(ymin = 85, ymax = 87, xmin = weight2$Date[1], xmax = weight2$Date[120]), fill = "grey80") +
-                geom_rect(aes(ymin = 87, ymax = 88, xmin = weight2$Date[1], xmax = weight2$Date[120]), fill = "grey90") +
+                geom_rect(aes(ymin = 85, ymax = 87, xmin = weight2$Date[1], xmax = weight2$Date[151]), fill = "grey80") +
+                geom_rect(aes(ymin = 87, ymax = 88, xmin = weight2$Date[1], xmax = weight2$Date[151]), fill = "grey90") +
                 geom_rect(aes(ymin = 85, ymax = 96, xmin = weight2$Date[101], xmax = weight2$Date[105]), fill = "moccasin") +
                 geom_point(shape = 21, fill = "cornflowerblue", colour = "blue", size = 6) + 
                 geom_smooth(method=loess, colour = "darkgreen", size = 2) + 
