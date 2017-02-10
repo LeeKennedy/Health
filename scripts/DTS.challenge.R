4cc041307d8680accb7d1aaf10c030be7438e617
LeeKennedy <mail@leekennedy.id.au>
2017-02-11 08:20:08 +1100

    Feb_11

--- scripts/DTS.challenge.R
+++ scripts/DTS.challenge.R
@@ -10,15 +10,15 @@ library(dplyr)
 weight <- read_excel("rawdata/Healthcheck.xlsx", sheet=1)
 weight2 <- weight %>%
         filter(Date >"2015-11-30") %>%
-        filter(Date <"2017-01-31")
+        filter(Date <"2017-02-28")
 cache("weight2")
 
 
 
 # Plot weight ------------------------------------------------------------ 
 plot_wt <- ggplot(weight2, aes(Date,Weight)) + 
-                geom_rect(aes(ymin = 85, ymax = 87, xmin = weight2$Date[1], xmax = weight2$Date[428]), fill = "grey80") +
-                geom_rect(aes(ymin = 87, ymax = 88, xmin = weight2$Date[1], xmax = weight2$Date[428]), fill = "grey90") +
+                geom_rect(aes(ymin = 85, ymax = 87, xmin = weight2$Date[1], xmax = weight2$Date[456]), fill = "grey80") +
+                geom_rect(aes(ymin = 87, ymax = 88, xmin = weight2$Date[1], xmax = weight2$Date[456]), fill = "grey90") +
                 geom_point(shape = 21, fill = "cornflowerblue", colour = "blue", size = 6) + 
                 geom_smooth(method=loess, colour = "darkgreen", size = 2) + 
                 geom_hline(yintercept = 95.3, lty=2, col="red") +
