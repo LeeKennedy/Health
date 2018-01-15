e74366a6afd0e0da4df92e87224d019aa6885d04
LeeKennedy <mail@leekennedy.id.au>
2018-01-15 19:28:12 +1100

    Adjusted to ignore the RH arm BP results.

--- scripts/BP.R
+++ scripts/BP.R
@@ -15,7 +15,7 @@ data1 <- select(data, everything())%>%
         mutate(Systolic = as.numeric(as.character(A)), 
                Diastolic = as.numeric(as.character(B)))
 
-data2 <- data1[,c(1,16,17)]
+data2 <- data1[,c(1,22,23)]
 data2 <- na.omit(data2)
 
 data2$Date <- ymd(data2$Date)
@@ -23,7 +23,7 @@ data2$Date <- ymd(data2$Date)
 data3 <- gather(data2, Type, Reading, -Date)
 
 # Plot blood pressure ----------------------------------------------------
-plot <- ggplot(data3, aes(Date, Reading, colour = Type)) +
+bp_plot <- ggplot(data3, aes(Date, Reading, colour = Type)) +
         geom_point(size=4, shape=21) +
         geom_smooth(lwd=1) +
         scale_colour_brewer(palette="Set1") +
@@ -31,5 +31,5 @@ plot <- ggplot(data3, aes(Date, Reading, colour = Type)) +
         theme(panel.grid.major = element_line(size = 0.5, color = "grey"), 
               axis.line = element_line(size = 0.7, color = "black"), 
               text = element_text(size = 14))
-plot
+bp_plot
 
